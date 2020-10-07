6165bc4db599f6e4268f6a329f8e036fa1f48cf1
Michael Hacker <hack1973@swbell.net>
2020-10-07 13:36:12 -0500

    completed fav-links

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fav-links',
+  selector: 'fav-links',
   templateUrl: './fav-links.component.html',
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
-
+  linksTitle = 'My Favorite Links';
+  favLinks = ['https://education.launchcode.org/', 'https://github.com/hack1973/', 'https://repl.it/@hack1973'];
+  
   constructor() { }
 
   ngOnInit() {
