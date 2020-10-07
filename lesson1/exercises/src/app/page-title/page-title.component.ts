0859ebd7be7f9d2dc4845f47cb5d65286b48338b
Michael Hacker <hack1973@swbell.net>
2020-10-07 14:13:22 -0500

    rearranged components

--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageTitleComponent implements OnInit {
   title = 'Angular Lesson 1 Exercises';
+  
   constructor() { }
 
   ngOnInit() {
