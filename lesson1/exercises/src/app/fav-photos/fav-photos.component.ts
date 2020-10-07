026109194ca078c0569cfb3333cb6f6c9f92386f
Michael Hacker <hack1973@swbell.net>
2020-10-07 12:22:05 -0500

    completed fav-photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lifestyle-bestdogbreeds-1577128927.png?crop=0.502xw:1.00xh;0.263xw,0&resize=640:*';
+  image2 = 'https://hips.hearstapps.com/edc.h-cdn.co/assets/16/25/gettyimages-563680559.jpg?crop=0.447xw:1.00xh;0.253xw,0&resize=480:*';
+  image3 = 'https://hgtvhome.sndimg.com/content/dam/images/hgtv/stock/2019/2/19/GettyImages-172955261.jpg.rend.hgtvcom.966.644.suffix/1550582942666.jpeg';
 
   constructor() { }
 
